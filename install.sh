35dc9f7d2b51e38fe2ec3ebf04ac527da1772f15
Mahdi Bchatnia <git@mahdi.jp>
2020-07-02 00:22:52 +0900

    Improved the launch agent

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 git submodule update --init --recursive
 
+brew install jq
+
 cd nature_remo
+sudo gem install bundler
 sudo bundle install # sudo ruby "I don't wanna be managing ruby versions right now"
 cd ..
 
 cp jp.mahdi.meishi-console.plist ~/Library/LaunchAgents/
-launchctl load jp.mahdi.meishi-console
+launchctl load ~/Library/LaunchAgents/jp.mahdi.meishi-console.plist
