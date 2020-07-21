238f6a4bb13115204e9e2735182ea13de5196af2
Mahdi Bchatnia <git@mahdi.jp>
2020-07-21 20:36:05 +0900

    Updated keymap

--- Sources/meishi-console/Key.swift
+++ Sources/meishi-console/Key.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 enum Key: String {
+    case f12
     case f13
-    case sf13
-    
     case f14
-    case sf14
-    
     case f15
-    case sf15
     
     case f16
-    case sf16
+    case f17
+    case f18
+    case f19
     
     init?(key: String) {
         guard let matchedCase = Key(rawValue: key.lowercased()) else {
@@ -23,17 +21,15 @@ enum Key: String {
     
     var command: String {
         switch self {
+        case .f12: return "ruby nature_remo/bin/natureremo aircon_off"
         case .f13: return "ruby nature_remo/bin/natureremo aircon_change_temperature -1"
-        case .sf13: return "ruby nature_remo/bin/natureremo aircon_off"
-
         case .f14: return "ruby nature_remo/bin/natureremo aircon_change_temperature 1"
-        case .sf14: return "ruby nature_remo/bin/natureremo aircon_on"
-            
-        case .f15: return "hue-lights-controller/hue.sh lightDimmer 1"
-        case .sf15: return "hue-lights-controller/hue.sh lightOff 1"
+        case .f15: return "ruby nature_remo/bin/natureremo aircon_on"
 
-        case .f16: return "hue-lights-controller/hue.sh lightBrighter 1"
-        case .sf16: return "hue-lights-controller/hue.sh lightOn 1"
+        case .f16: return "hue-lights-controller/hue.sh lightOff 1"
+        case .f17: return "hue-lights-controller/hue.sh lightDimmer 1"
+        case .f18: return "hue-lights-controller/hue.sh lightBrighter 1"
+        case .f19: return "hue-lights-controller/hue.sh lightOn 1"
         }
     }
     
